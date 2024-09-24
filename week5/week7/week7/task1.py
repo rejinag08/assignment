036018757b1d44a77da804628a2e1a0bac38d9e2
rejina gautam <Rejina.Gautam@metropolia.fi>
2024-09-24 16:00:01 +0300

    i have done my week 7 homework

---
+++ week5/week7/week7/task1.py
@@ -0,0 +1,15 @@
+import random
+
+def roll_dice():
+    return random.randint(1, 6)
+
+def main_dice_roll():
+    result = 0
+    rolls = []  # To keep track of all roll results
+    while result != 6:
+        result = roll_dice()
+        rolls.append(result)
+        print("rolls:", result)
+    return rolls
+
+main_dice_roll()
---
+++ week5/week7/week7/task3.py
@@ -0,0 +1,10 @@
+def sum_of_list(numbers):
+    return sum(numbers)
+
+def main_sum_test():
+    test_list = [10, 20, 30, 40, 50]
+
+    total_sum = sum_of_list(test_list)
+
+    print(f"The sum of {test_list} is {total_sum}.")
+main_sum_test()
