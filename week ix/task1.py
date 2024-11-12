e6b186a6b7ada1505461f756aee3e566a39808ed
rejina gautam <Rejina.Gautam@metropolia.fi>
2024-11-12 12:49:11 +0200

    i have done

---
+++ week ix/task1.py
@@ -0,0 +1,9 @@
+def sum_of_list(numbers):
+    return sum(numbers)
+
+def main_sum_test():
+    test_list = [10, 20, 30, 40, 50]
+
+    total_sum = sum_of_list(test_list)
+
+    print(f"The sum of {test_list} is {total_sum}.")
